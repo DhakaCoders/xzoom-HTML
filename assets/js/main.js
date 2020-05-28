fbc82d5606ac9c5cd16a3d1e9ec34282f4dd30e1
monirulislamweb <65311288+monirulislamweb@users.noreply.github.com>
2020-05-29 01:58:37 +0600

    desktop view done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	$('.nav-items-hass-submenu').click(function(){
 		$('.submenu-items').toggleClass('js-submenu-items');
+		$('.nav-items-hass-submenu').toggleClass('js-nav-items-hass-submenu');
 	});
 	$('.slick-bnr-slide').slick({
         autoplaySpeed: 3000,
@@ -8,5 +9,7 @@ $(document).ready(function(){
         autoplay: true,
         dots: true
       });
-	
+	$('.select-drpdwn').click(function(){
+		$('.select-drpdwn').toggleClass('js-select-drpdwn');
+	});
 });
