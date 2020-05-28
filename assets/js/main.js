d0eadfd37fe5b25f2ffd1a7841e237f057e3feed
monirulislamweb <65311288+monirulislamweb@users.noreply.github.com>
2020-05-28 21:44:49 +0600

    slider added

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,11 @@ $(document).ready(function(){
 	$('.nav-items-hass-submenu').click(function(){
 		$('.submenu-items').toggleClass('js-submenu-items');
 	});
+	$('.slick-bnr-slide').slick({
+        autoplaySpeed: 3000,
+        arrows: false,
+        autoplay: true,
+        dots: true
+      });
+	
 });
